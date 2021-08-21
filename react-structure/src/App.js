71d2be8a530b6758606f2c152528e86119230483
Ibrahim Ali <hemamessi47@gmail.com>
2021-08-21 10:36:44 +0200

    import bootstrap

--- react-structure/src/App.js
+++ react-structure/src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-import First from './First'
+import First from './First';
 
 function App() {
   return (
     <div className="App">
       <>
-      <h1>This is first words from app.js</h1>
+      <h1 className="text-primary bg-dark">This is first words from app.js</h1>
       <First />
       </>
     </div>
