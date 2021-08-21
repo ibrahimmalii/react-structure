8b36fe51024a442ad46bff5f76e36de31e38dc6b
Ibrahim Ali <hemamessi47@gmail.com>
2021-08-21 10:08:27 +0200

    console for props

--- react-structure/src/Button.js
+++ react-structure/src/Button.js
@@ -1,4 +1,6 @@
 const Button = (props) =>{
+    //props is a built in param in react => passed from parent to child 
+    console.log(props);
     return (
         <button onClick={props.whileClick}>{props.content}</button>
     )
