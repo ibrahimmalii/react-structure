4c8ab478abb14fc3cc7ada7234ee9e51f1b8c712
Ibrahim Ali <hemamessi47@gmail.com>
2021-08-21 09:23:07 +0200

    add function and class component

---
+++ react-structure/src/First.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class First extends React.Component{
+    render(){
+        return (
+            <h1>This is first test from class component</h1>
+        )
+    }
+}
---
+++ react-structure/src/FunFirst.js
@@ -0,0 +1,7 @@
+const FunFirst = () =>{
+    return (
+        <h1>This is first function component</h1>
+    )
+}
+
+export default FunFirst;
